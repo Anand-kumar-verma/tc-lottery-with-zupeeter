5ef7f2536f7656a1978e1406eae339e6b1515548
sushmasingh2000 <sushmasingh16166@gmail.com>
2024-06-18 15:54:04 +0530

    nbhjfvg

--- src/pages/wallet/Wallet.js
+++ src/pages/wallet/Wallet.js
@@ -14,6 +14,7 @@ import {
   checkTokenValidity,
   depositHistoryFunction,
   getBalanceFunction,
+  showRank,
 } from "../../services/apiCallings";
 import { rupees } from "../../services/urls";
 import CustomCircularProgress from "../../shared/loder/CustomCircularProgress";
@@ -175,7 +176,7 @@ function Wallet() {
              color="initial"
              className="!text-white"
            >
-            Rank : {profile?.rec?.Club}
+            Rank : {showRank(profile?.rec?.Club)}
             
            </Typography>
             }
