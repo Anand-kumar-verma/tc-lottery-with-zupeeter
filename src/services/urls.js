239ffb931971e80c83a2510ed5d7d253b4d8f2ff
sushmasingh2000 <sushmasingh16166@gmail.com>
2024-07-31 17:14:49 +0530

    jhf

--- src/services/urls.js
+++ src/services/urls.js
@@ -4,7 +4,7 @@ export const domain = "https://zupeeter-timer-27jp.onrender.com/"
 export const domain_ovi_node = "https://api.zupeeter.com";
 // export const domain_ovi_node = "https://zupeegame.info"
 // export const domain_ovi_node = "http://192.168.85.149:8000"
-export const newdomain1 = "http://192.168.18.183:9002";
+// export const newdomain1 = "http://192.168.18.183:9002";
 export const rupees = "₹";
 export const zubgback = "#F48901";
 
