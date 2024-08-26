3aa4a0259f0ddaf5b3a5dc639e889d066822c37a
anand <vermaanand278@gmail.com>
2024-08-26 12:49:15 +0530

    dsf

--- src/GamePage/AllBets.js
+++ src/GamePage/AllBets.js
@@ -4,40 +4,15 @@ import axios from "axios";
 import React, { useEffect, useState } from "react";
 import toast from "react-hot-toast";
 import { endpoint } from "../services/urls";
+import { useSocket } from "../shared/socket/SocketContext";
 
 const AllBets = ({ formik, fk }) => {
-  const [isAvailable, setIsAvailable] = useState([]);
   const [allbetsdata, setAllBetsData] = useState([]);
   const [isLoading, setisLoding] = useState(false);
-  // useEffect(() => {
-  //   setDisplayedData([]);
-  //   setIsAvailable([]);
-  //   if (allbetsdata) {
-  //     fetchData();
-  //   }
-  // }, [allbetsdata, formik.values.refetch]);
+  const socket = useSocket();
 
-  // const fetchData = async () => {
-  //   const newData = allbetsdata;
-  //   for (let i = 0; i < newData.length; i++) {
-  //     !fk?.values?.isFlying ||
-  //       (displayedData?.length < 20 &&
-  //         setDisplayedData((prevData) => [...prevData, newData[i]]));
-  //     setIsAvailable((prevData) =>
-  //       isAvailable.length < 6
-  //         ? [...prevData, Math.floor(Math.random() * 100) + 1]
-  //         : [...prevData]
-  //     );
-  //     await new Promise((resolve) =>
-  //       setTimeout(resolve, Math.floor(Math.random() * 1000) + 300)
-  //     );
-  //   }
-  // };
   const getAllBets = async () => {
     try {
-      // const response = await axios.get(
-      //   `${endpoint.total_bet_history}`
-      // );
       const response = await axios.get(endpoint.node_api.get_leder_data);
       setAllBetsData(response?.data?.data || []);
     } catch (e) {
@@ -48,6 +23,18 @@ const AllBets = ({ formik, fk }) => {
 
   useEffect(() => {
     getAllBets();
+  }, []);
+
+  useEffect(() => {
+    const handleBet = (data) => {
+      // setcrashed(data);
+      console.log(data);
+    };
+
+    socket.on("user_bet", handleBet);
+    return () => {
+      socket.off("user_bet", handleBet);
+    };
   }, []);
 
   if (isLoading)
@@ -74,7 +61,6 @@ const AllBets = ({ formik, fk }) => {
           <div
             key={index}
             className={`${
-             
               i?.multiplier
                 ? "bg-[#213519] bg-opacity-30 border-[2px] border-[#1e430ff6]"
                 : "bg-black bg-opacity-30"
@@ -88,30 +74,28 @@ const AllBets = ({ formik, fk }) => {
                   sx={{ width: 24, height: 24, fontSize: 10 }}
                 />
                 <span className="text-[10px] text-gray-500">
-                  {(i?.email?.substring(0, 1)||"*") +
+                  {(i?.email?.substring(0, 1) || "*") +
                     "**" +
-                    (i?.email?.substring(1, 2)||"*")}
+                    (i?.email?.substring(1, 2) || "*")}
                 </span>
               </p>
             </div>
             <div className="flex gap-2 items-center">
-              <span
-                className={`text-[10px] text-gray-500`}
-              >
+              <span className={`text-[10px] text-gray-500`}>
                 {Number(i?.amount || 0)?.toFixed(2)}
               </span>
               <span
-                    className={`bg-black rounded-full px-3 py-1 text-[10px] 
+                className={`bg-black rounded-full px-3 py-1 text-[10px] 
                `}
-                  >
-                    {Number(i?.multiplier || 0)?.toFixed(2)}x
-                  </span>
+              >
+                {Number(i?.multiplier || 0)?.toFixed(2)}x
+              </span>
             </div>
             <div className="flex w-full justify-end">
               <div className="flex gap-2 items-center">
-              <span className={`text-[10px]  text-white`}>
-                      {Number(i?.amountcashed || 0)?.toFixed(2)}
-                    </span>
+                <span className={`text-[10px]  text-white`}>
+                  {Number(i?.amountcashed || 0)?.toFixed(2)}
+                </span>
               </div>
             </div>
           </div>
